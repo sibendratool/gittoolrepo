21b20f4a80627463793f3f3466079c8189b4a308
deben <deben@LAPTOP-SGE3RLFA>
2022-07-15 14:48:53 +0530

    test code changed by dev1

--- MyCode/src/com/nt/test/Test.java
+++ MyCode/src/com/nt/test/Test.java
@@ -1,7 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Calculator;
+
 public class Test {
 	public static void main(String[] args) {
-		
+		Calculator cal = new Calculator();
+		int sum = cal.add(10, 20);
+		System.out.println("Sum is: "+sum);
 	}
 }
