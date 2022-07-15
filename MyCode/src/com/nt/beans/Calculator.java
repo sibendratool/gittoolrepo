2340e623771abdbc4f90e9639a8a0f35e6272302
deben <deben@LAPTOP-SGE3RLFA>
2022-07-15 15:22:41 +0530

    In Calculator.java multiply() modified by TL

--- MyCode/src/com/nt/beans/Calculator.java
+++ MyCode/src/com/nt/beans/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 	public int sub(int a, int b) {
 		return a-b;
 	}
-	public int multiply(int a, int b) {
-		return a*b;
+	public int multiply(int a, int b,int c) {
+		return a*b*c;
 	}
 }
